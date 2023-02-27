b5c71ca1b4f4042853c742a572bf298ace7bc6cf
Victor Okeke <okekevicktur@gmail.com>
2023-02-27 02:21:21 +0100

    general countries

--- asset/app.js
+++ asset/app.js
@@ -5,39 +5,71 @@ const countryFlag= document.getElementById("country-flag");
 const countryName= document.getElementById("name");
 const population= document.getElementById("population");
 const capital= document.getElementById("capital");
+const countries = document.querySelector(".countries");
+
+ // slizedArray.forEach(slicey => {
+    // countryFlag.setAttribute('src', `${slizedArray[0].flags.svg}`);
+    // countryName.innerText=`${slizedArray[0].name.common}`;
+    // population.innerText=`${slizedArray[0].population}`;
+    // capital.innerText=`${slizedArray[0].capital[0]}`;
 
 
 async function getapi(url){
         //storing response 
-    try {
+    // try {
             const response = await fetch(`${url}`);
             if (response.ok) {
                 //storing data in form of JSon
                 var data = await response.json();
-                // const element = data;
-                // console.log(element);
-                const slizedArray = data.slice(0,8);
-                //console.log(slizedArray);
-                // slizedArray.forEach(slicey => {
-                countryFlag.setAttribute('src', `${slizedArray[0].flags.svg}`);
-                // console.log(slizedArray[0].flags.svg)
-                countryName.innerText=`${slizedArray[0].name.common}`;
-                // console.log(slizedArray[0].name.common)
-                population.innerText=`${slizedArray[0].population}`;
-                // console.log(slizedArray[0].population)
-                // console.log(slizedArray[0].capital[0]);
-                capital.innerText=`${slizedArray[0].capital[0]}`;
+                const slizedArray = data.slice(1,25);
+               
 
-                // });
+                for (let i = 0; i < slizedArray.length; i++) {
+                    var Countryframe= document.createElement('div');
+                    Countryframe.className ="country";
+
+                    var cardWrap = document.createElement('div');
+                    cardWrap.className= "imgwrap";
+                    //img element
+                    var card = document.createElement('img');
+                    card.className= "country-flag";
+                    card.src =`${slizedArray[i].flags.png}`;
+                    
+                    //country details Container
+                    var detailsWrap = document.createElement('div');
+                    detailsWrap.className = "country-details";
+                    //Children
+                    var name = document.createElement('p');
+                    name.className= "name";
+                    name.innerText  = `${slizedArray[i].name.common}`;
+                    //population
+                    var population = document.createElement('p');
+                    population.className= "population";
+                    population.innerText  = `${slizedArray[i].population}`;
+                    //capital
+                    var capital = document.createElement('p');
+                    capital.className= "capital";
+                    capital.innerText  = `${slizedArray[i].capital[0]}`;
+
+                    // append child
+                    detailsWrap.appendChild(name); 
+                    detailsWrap.appendChild(population);
+                    detailsWrap.appendChild(capital);
+                    cardWrap.appendChild(card);
+                    Countryframe.appendChild(cardWrap);
+                    Countryframe.appendChild(detailsWrap);
+                    countries.appendChild(Countryframe);
+
+                }
             }
             else{
             alert("word not available");
             
             }
     
-    } catch (error) {
-        alert("Opps... it's hard getting the meaning");       
-    }     
+    // } catch (error) {
+    //     alert("Opps... it's hard getting the meaning");       
+    // }     
 } 
 
 search.addEventListener('click', function(){
